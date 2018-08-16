c752fc54c0cc6a8420c0a2b2c86a94075b95e751
Nabil M <nmiri127@gmail.com>
2018-08-16 09:29:13 -0230

    Added set_terminate handler

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+
+	std::set_terminate([](){ qFatal("An unhandled exception has occured. Please contact nabil.miri@dfo-mpo.gc.ca for technical support."); });
+
 	const QApplication a(argc, argv);
 
 	MainWindow w;
