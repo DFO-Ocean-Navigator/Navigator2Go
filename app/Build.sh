6c40b8bf4283da7bef41fc61a8483ce96308a356
Nabil M <nmiri127@gmail.com>
2018-11-09 15:10:06 -0330

    Edit build script to ingest path to Qt static directory
    * For Jenkins CI.

--- app/Build.sh
+++ app/Build.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-/home/nabil/Qt/static/bin/qmake -config release
+if [ -z "$1" ]; then
+    echo "Enter path to static Qt bin directory"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "Directory doesn't exist!"
+    echo $1
+    exit 1
+fi
+
+$1/qmake -config release
 make -j12 clean
 make -j12
 make -j12 clean
