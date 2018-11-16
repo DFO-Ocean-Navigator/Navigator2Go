594e388cfa6fb089e661f99a1334e7a4b631fb3e
Nabil M <nmiri127@gmail.com>
2018-11-16 15:29:40 -0330

    Minor changes

--- app/datadownloaddesc.h
+++ app/datadownloaddesc.h
@@ -10,7 +10,7 @@
 /// Holds info needed to download a dataset from the remote server.
 struct NODISCARD DataDownloadDesc {
 
-	NODISCARD auto operator==(const DataDownloadDesc& rhs) const Q_DECL_NOTHROW {
+	NODISCARD auto operator==(const DataDownloadDesc& rhs) const noexcept {
 		return ID == rhs.ID;
 	}
 
@@ -24,7 +24,7 @@ struct NODISCARD DataDownloadDesc {
 
 /***********************************************************************************/
 /// Overload qHash to allow storage in a QHash table.
-NODISCARD inline auto qHash(const DataDownloadDesc& dlData) Q_DECL_NOTHROW {
+NODISCARD inline auto qHash(const DataDownloadDesc& dlData) noexcept {
 	return qHash(dlData.ID);
 }
 
