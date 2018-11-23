1f97f3c518656c1bc652c159cf0c7fb06c7d656d
Nabil M <nmiri127@gmail.com>
2018-11-23 09:38:29 -0330

    Update docs

--- docs/class_server_manager.js
+++ docs/class_server_manager.js
@@ -1,10 +1,12 @@
 var class_server_manager =
 [
     [ "ServerManager", "class_server_manager.html#ab81e5fc9bd5399ca7ba924f104ee3a1f", null ],
+    [ "ServerManager", "class_server_manager.html#a85eced84b80824ff8069752a843930f9", null ],
     [ "~ServerManager", "class_server_manager.html#a6e466d78b0a02dcbcdbf6372c19909f3", null ],
     [ "isTHREDDSRunning", "class_server_manager.html#a260a4870fddc6712d0d45b455b8f346a", null ],
     [ "isWebUIRunning", "class_server_manager.html#a34fa1799eed16534401cdc8a8709d2c4", null ],
     [ "refreshServers", "class_server_manager.html#aa0b543dcfaf1455dab421d22c7fae38b", null ],
+    [ "setEnvironment", "class_server_manager.html#a616832619f3da4030166f0df3ed61925", null ],
     [ "startServers", "class_server_manager.html#a8a6dc6c2f65cc7bcfc91b660ed341daf", null ],
     [ "startTHREDDS", "class_server_manager.html#a0674101d67efe81e7f1a22b6bc05d08c", null ],
     [ "startWebServer", "class_server_manager.html#a1e6cfbe01622713a90547ebdde4a664b", null ],
